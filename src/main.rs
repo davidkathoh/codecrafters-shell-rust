405a1664eba759b09e2aa73c130145021949d131
David Kathoh <mudavidkat@gmail.com>
2025-01-04 13:58:09 +0200

    implemnted  repl

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    
 
     // Wait for user input
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         println!("{}: command not found",input.trim()); 
+        
+        
     }
    
 }
