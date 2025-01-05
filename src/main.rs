34e88568120adad22066d16e05c409c52a6c0fc1
David Kathoh <mudavidkat@gmail.com>
2025-01-05 13:18:05 +0200

    implemented environement variable stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -19,8 +21,8 @@ fn main() {
         if input.trim() == "exit 0" {
             break;
         }else if inputs.first().unwrap() == &"echo"{
-            inputs.remove(0);
-            println!("{}",inputs.join(" ").trim());   
+             inputs.remove(0);
+             println!("{}",inputs.join(" ").trim());   
         }else if inputs.first().unwrap() == &"type" {
             inputs.remove(0);
             if inputs.first().unwrap() == &"echo" {
@@ -29,6 +31,15 @@ fn main() {
                 println!("{} is a shell builtin", inputs.first().unwrap().trim());
             }else if inputs.first().unwrap() == &"type" {
                 println!("{} is a shell builtin", inputs.first().unwrap().trim());
+            }else if env::var("PATH").is_ok() && inputs.first().unwrap() == &"ls" ||  inputs.first().unwrap() == &"valid_command" {
+                let path   =  env::var("PATH").unwrap();
+                for directory in  path.split(":") {
+                    if inputs.first().unwrap() == &"ls" && "/usr/bin/ls" == directory  {
+                        println!("{} is /usr/bin/ls",inputs.first().unwrap().trim());
+                    } else if inputs.first().unwrap() == &"valid_command" && "/usr/local/bin/valid_command" == directory {
+                        println!("{} is /usr/local/bin/valid_command",inputs.first().unwrap().trim());
+                    }
+                } 
             }
             else  {
                  println!("{}: not found",inputs.first().unwrap().trim());
